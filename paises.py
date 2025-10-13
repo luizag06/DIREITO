a8071ff32d4b48694cefb50386de3c2b3405ba89
luizag06 <luiza.guim06@gmail.com>
2025-10-13 15:13:08 -0300

    Update paises.py

--- paises.py
+++ paises.py
@@ -10,5 +10,8 @@ fig= px.scatter_geo(dataset,
                     hover_name=dataset['nome'])
 fig.update_layout(title='Cordenadas dos países no mapa',
                   geo_scope='world')
-fig.show()
+fig.update_layout(title= 'mapa coroplético dos países', 
+                  geo_scope='world') 
+st.plotly_chart(fig, use_container_width=True, theme="streamlit")
+
 
